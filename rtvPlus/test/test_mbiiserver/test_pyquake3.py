3f55fade5944d9f59f0f4ea68bf24812c7caac84
Fabian Junge <fabsjunge@gmail.com>
2018-03-14 23:13:57 -0500

    adding unittest framework

---
+++ rtvPlus/test/test_mbiiserver/test_pyquake3.py
@@ -0,0 +1,20 @@
+"""Unit tests for rtvPlus.mbiiserver.pyquake3.py
+"""
+
+from unittest import TestCase
+# import rtvPlus.mbiiserver.pyquake3 as pyquake3
+
+
+class TestSetServer(TestCase):
+    """
+    Test the set_server function in pyquake3
+    """
+
+    def setUp(self):
+        pass
+
+    def test_fail(self):
+        self.assertTrue(False)
+
+    def tearDown(self):
+        pass
