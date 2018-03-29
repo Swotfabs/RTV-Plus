5eb4eec34cac67bdadce580f1d073919d3d5b9f6
Fabian Junge <fabsjunge@gmail.com>
2018-03-29 12:24:01 -0500

    adding parser and one-command functionality

--- rtvplus.py
+++ rtvplus.py
@@ -12,9 +12,15 @@ class RTVPrompt(Cmd):
     """This is a custom prompt that allows command line users to send
     rcon commands to the server"""
 
-    def __init__(self):
+    def __init__(self, server=None, rconpassword=None):
         super().__init__()
-        self.mbiiserver = None
+        if server:
+            print("Connecting to {} with rconpassword {}".format(server,
+                                                                 rconpassword))
+            self.mbiiserver = mbiiserver(server, rconpassword)
+        else:
+            print("Starting unconnected")
+            self.mbiiserver = None
 
     def cmdloop(self, intro=None):
         try:
@@ -35,14 +41,24 @@ class RTVPrompt(Cmd):
         except Exception as e:
             print(e)
 
+    def do_login(self, line):
+        if not self.mbiiserver:
+            print("Not currently connected to a server")
+            return
+        self.mbiiserver.server.set_rcon_password(line)
+
     def do_connect(self, line):
         """Connect to an MBII Server
         Usage: connect ip:port rconpassword"""
         if self.mbiiserver:
             print("Already connected, please disconnect first")
             return
-        server, rconpassword = line.split()
-        self.mbiiserver = mbiiserver(server, rconpassword)
+        try:
+            server, rconpassword = line.split()
+        except ValueError as value:
+            self.mbiiserver = mbiiserver(line, None)
+        else:
+            self.mbiiserver = mbiiserver(server, rconpassword)
 
     def do_disconnect(self, line):
         """Disconnects from the server"""
@@ -87,20 +103,55 @@ class RTVPrompt(Cmd):
 
 
 def add_arguments(parser):
-    parser.add_argument('mode', choices=['cmd', 'rcon'], help=(
-            "cmd launches a command line interface,"
-            " rcon sends one command to the server"))
+    parser.add_argument('mode', choices=['cmd', 'command'], help=(
+            "'cmd' launches a command line interface,"
+            " 'command' sends one command to the server"))
+    parser.add_argument('--server', '-s',
+                        help="the server to connect to in the form of ip:port")
+    parser.add_argument('--rconpassword', '-rcon',
+                        help="the rcon password for the server")
+    parser.add_argument('--command', '-com',
+                        help=("The command to send to the server, do not"
+                              " prefix with 'rcon'"))
+    parser.add_argument('--retries', '-ret', default=1,
+                        help=("in command mode, the number of times"
+                              " to attempt sending the command. The default is 1"))
+
+
+def main():
     pass
+    parser = argparse.ArgumentParser(
+        description="A tool to communicate with a pyquake3 server via rcon",
+        formatter_class=argparse.RawTextHelpFormatter,
+        epilog=(
+            "In cmd mode specifying --server will start the command prompt"
+            " connected to that server.\nSpecifying --rconpassword as well"
+            " will start the command prompt logged in to that server.\n"
+            "In command mode --server, --rconpassword, and --command are"
+            " required."))
+    add_arguments(parser)
+    args = parser.parse_args()
+
+    if args.mode == 'cmd':
+        prompt = RTVPrompt(server=args.server, rconpassword=args.rconpassword)
+        prompt.prompt = "> "
+        prompt.cmdloop()
+    elif args.mode == 'command':
+        if not args.server and args.rconpassword and args.command:
+            print("You must set the server and the password for command mode")
+            return
+        server = mbiiserver(args.server, args.rconpassword)
+        while args.retries:
+            try:
+                response_type, response_data = server.server.rcon(args.command)
+                print(response_data)
+            except Exception as e:
+                print(e)
+            finally:
+                args.retries -= 1
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="A tool to communicate with a pyquake3 server via rcon")
-    add_arguments(parser)
-    args = parser.parse_args()
-    print(args)
-    # prompt = RTVPrompt()
-    # prompt.prompt = "> "
-    # prompt.cmdloop("Starting prompt...")
+    main()
 else:
     raise NotImplementedError  # This module is not intedned to be imported
